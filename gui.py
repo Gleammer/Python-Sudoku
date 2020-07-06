3cb8ec62fe3f0b381a0ddac64bb59f01448dc27a
Radu Chirilov <radulin4a@gmail.com>
2020-07-06 03:34:01 +0300

    Finished the GUI but still needs some cosmetics

--- gui.py
+++ gui.py
@@ -1,30 +1,17 @@
 from tkinter import *
+from sudoku import *
 root = Tk()
 
-large_font = ('Verdana',30)
-maze = [
-    [0,3,8,0,6,0,4,1,0],
-    [0,0,0,4,7,0,0,2,0],
-    [2,0,9,0,0,0,0,6,0],
-    [0,0,5,8,0,2,3,9,0],
-    [0,0,0,0,0,4,1,0,7],
-    [9,1,0,3,5,0,2,0,0],
-    [8,9,4,0,0,5,6,7,0],
-    [5,0,7,0,4,6,8,0,0],
-    [0,0,3,0,0,0,5,0,9]
-]
-
-def show():
-    for i in maze:
-        for j in i:
-            print(j, end=" ")
-        print()
+large_font = ('Verdana', 30)
 
 def get_maze():
     for i in range(0, 9):
         for j in range(0, 9):
-            command = "maze[" + str(i) + "][" + str(j) + "] = in" + str(i) + str(j) + ".get()"
+            command = "maze[" + str(i) + "][" + str(j) +  "] = in" + str(i) + str(j) + ".get()"
             exec(command)
+            if maze[i][j]=='':
+                maze[i][j]=0
+            maze[i][j]=int(maze[i][j])
 
 for i in range(0,9):
     for j in range(0,9):
@@ -35,10 +22,10 @@ for i in range(0,9):
 
 def get_values():
     get_maze()
+    solve()
     show()
 
-
-myButton = Button(root, text="Get the maze", command=get_values)
+myButton = Button(root, text="Solve", command=get_values)
 myButton.grid(row=9)
 
 root.mainloop()
